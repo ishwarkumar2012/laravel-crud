40003798253fc4e24cdcac78bca7d74eb2adb36c
Ishwar Kumar Gupta <ishwargupta2012@gmail.com>
2016-09-01 08:20:26 +0545

    added user model to user list

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\User;
 use Illuminate\Http\Request;
 
 use App\Http\Requests;
@@ -16,7 +17,7 @@ class UserController extends Controller
 
     function users()
     {
-
+        $users = User::all();
         return view('users.list');
     }
 
